792c5a3edb003e29f2f2b517208478ce2758d8c7
mega_it <.>
2024-09-05 20:43:57 +0900

    [배경민-학원]  - 로그인, 회원가입 화면 마크업  - thymeleaf layout 라이브러리 적용
    #2

---
+++ src/main/java/com/kyung/memo/user/UserController.java
@@ -0,0 +1,20 @@
+package com.kyung.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/join-view")
+	public String inputJoin(){
+		return "user/join";
+	}
+	
+	@GetMapping("login-view")
+	public String inoutLogin(){
+		return "user/login";
+	}
+}
