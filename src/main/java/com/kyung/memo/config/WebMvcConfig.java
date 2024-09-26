f94c974b478322bc9c604984381c9f807a06b715
mega_it <.>
2024-09-26 20:10:38 +0900

    [배경민]  - 로그인 되지 않은 상태라면 post에 접근하지 못하도록 인터셉트 설정  - 로그인 된 상태라면 로그인 화면으로 접근하지 못하도록 인터셉트 설정

--- src/main/java/com/kyung/memo/config/WebMvcConfig.java
+++ src/main/java/com/kyung/memo/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.kyung.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kyung.memo.common.FileManager;
+import com.kyung.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -13,4 +15,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/"); // Mac이나 리눅스는 //, 윈도우는 ///
 	}
+	
+	// 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		PermissionInterceptor interceptor = new PermissionInterceptor();
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**") // 이 경로를 거치는 것만 인터셉터를 거치게 하기
+		.excludePathPatterns("/static/**", "/images/**", "/user/logout"); // 이 경로들은 인터셉터를 거치지 않고 가도록 함
+	}
+		
 }
